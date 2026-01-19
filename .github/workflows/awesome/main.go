ec80c2d21c9daa28c8e9f0dbccc6947ed8ec1dd1
Dalia Abuadas <dmabuada@users.noreply.github.com>
2026-01-19 11:58:19 -0500

    Update TODO comment in main.go

--- .github/workflows/awesome/main.go
+++ .github/workflows/awesome/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	github_go_script.Call(func(options *github_go_script.Options) (map[string]string, error) {
-		// TODO: Replace this with your code
+		// TODO: Replace this with your code 
 		repo, _, err := options.Client.Repositories.Get(options.Ctx, options.Context.Repository.OwnerName, options.Context.Repository.Name)
 		if err != nil {
 			return nil, err
